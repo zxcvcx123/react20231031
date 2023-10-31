08e538a69cbd45e52be265f903a36a039c1e7af2
JungMo <jmjm0611@naver.com>
2023-10-31 12:01:31 +0900

    예제1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@
 function App() {
   return (
     <div>
-
+    <MyComponent1/>
+    <MyComponent2/>
     </div>
   );
 }
 
+function MyComponent1(){
+  return (
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+  )
+}
+
+function MyComponent2(){
+  return (
+      <>
+        <p>lorem ipsum great</p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>great</li>
+        </ul>
+      </>
+  )
+}
 export default App;
