0b408f147c38ffefd74e05fab117a5cf772f928a
JungMo <jmjm0611@naver.com>
2023-11-01 16:28:57 +0900

    보통의 css파일 module 예제 완료

--- src/component/MyContainer.js
+++ src/component/MyContainer.js
@@ -2,7 +2,9 @@ import mystyle from "../css/style2.module.css";
 export function MyContainer() {
   return (
     <div>
-      <h1 className={mystyle.boxClass}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${mystyle.boxClass} ${mystyle.error} ${mystyle.note}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
