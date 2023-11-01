34498c952189c93b17f5fb018dfed3052ca04328
JungMo <jmjm0611@naver.com>
2023-11-01 16:36:46 +0900

    보통의 css파일 module 예제 완료

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -3,7 +3,9 @@ export function MyText() {
   return (
     <div>
       {/* red 배경색, white 글자색 */}
-      <h1 className={ms.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${ms.error} ${ms.coffee} ${ms.special}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
