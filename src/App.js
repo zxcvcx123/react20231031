cd1a6d88cdc59327da12672dd1792c6aef7f120d
JungMo <jmjm0611@naver.com>
2023-11-01 11:48:05 +0900

    export, import 연습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+// export된 컴포넌트(변수, 함수, 객체 등등) 를 import 해서 사용
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+
 function App() {
   return (
     <>
-      <MyComp name={"흥민"} age={33} address={"신촌"} city={"서울"} />
-      <MyComp age={22} address={"강남"} city={"서울"} />
+      <MyElem />
+      <MyComp />
     </>
-  );
-}
-
-function MyComp({ name = "강인", address, city, age = 10 }) {
-  return (
-    <div>
-      <p>
-        {name} 은 {age}세 이고 {address}, {city}에 산다.
-      </p>
-    </div>
   );
 }
 
---
+++ src/component/MyComp.js
@@ -0,0 +1,7 @@
+export function MyComp() {
+  return (
+    <div>
+      <p>코드가 너무 길어짐</p>
+    </div>
+  );
+}
