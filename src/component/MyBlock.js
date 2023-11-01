405381775426684bbd600ea834f919097a571da8
JungMo <jmjm0611@naver.com>
2023-11-01 16:15:50 +0900

    보통의 css파일 module 예제 완료

---
+++ src/component/MyBlock.js
@@ -0,0 +1,9 @@
+import ys from "../css/yourStyle.module.css";
+
+export default function MyBlock() {
+  return (
+    <div>
+      <h1 className={ys.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,9 +1,9 @@
-import "../css/mystyle.css";
+import ms from "../css/mystyle.module.css";
 export function MyText() {
   return (
     <div>
       {/* red 배경색, white 글자색 */}
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      <h1 className={ms.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
