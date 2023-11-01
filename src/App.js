396a2d0f2b1a2f627474a3c9c0f199430e698dab
JungMo <jmjm0611@naver.com>
2023-11-01 10:32:01 +0900

    컴포넌트 property 구조분해 할당으로 인자 전달

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 function App() {
   return (
     <>
-      <h1
-        className={"note"}
-        style={{ backgroundColor: "blue", color: "white" }}
-        title={"제목 요소"}
-      >
-        lorem.
-      </h1>
-      <p title={"문단 요소"}>Lorem ipsum dolor.</p>
-      {/* 컴포넌트의 property는 함수 인자로 넘어간다 */}
-      <MyComp title={"내 컴포넌트"} name={"흥민"} />
-      <MyComp title={"I am 컴포넌트"} name={"강인"} />
+      <MyComp value={"흥민"} address={"신촌"} />
     </>
   );
 }
 
-// 함수 인자로
-// 사용한 곳에서 넘긴 property들이 객체형태로 담김
-// 예) title={"내 컴포넌트"} name={"흥민"} => {title: "내 컴포넌트", name: "흥민"} 형태로 담김
-// props라고 불린다
-function MyComp(props) {
-  console.log("MyComp가 받은 props", props);
+function MyComp({ value, address }) {
   return (
     <div>
-      <h1 title={props.title}>hello {props.name}</h1>
-      <h1>{props.name}</h1>
+      <h1>
+        {value}는 {address}에 산다
+      </h1>
     </div>
   );
 }
