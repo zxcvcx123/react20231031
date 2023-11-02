09e212d92cb9811d37defca353223d9127c56232
JungMo <jmjm0611@naver.com>
2023-11-02 09:34:19 +0900

    필요 없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-// 보통 css 파일
-// css module 사용
-// chakraui css library
-
-import { MyComp } from "./component/MyComp";
-import { MyBox } from "./component/MyBox";
-import { MyContainer } from "./component/MyContainer";
-
 function App() {
-  return (
-    <>
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/component/MyComp.js
+++
@@ -1,9 +0,0 @@
-export function MyComp() {
-  return (
-    <div>
-      <h1 style={{ color: "gold", backgroundColor: "blue" }}>
-        Lorem ipsum dolor.
-      </h1>
-    </div>
-  );
-}
