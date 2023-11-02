d43c43e7334c93d9900f2975e3e5743969909fa7
JungMo <jmjm0611@naver.com>
2023-11-02 16:30:35 +0900

    Nav 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
 import React from "react";
 import {
   Box,
-  Center,
+  Button,
   Flex,
-  FormControl,
-  FormLabel,
-  Image,
-  Switch,
-  Textarea,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
 } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Center>
-        <Box w={"480px"}>
-          <Image src="https://bit.ly/dan-abramov" />
-        </Box>
-        <Box>
-          <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
-        </Box>
-        <Box>
-          <Image boxSize={"200px"} src="https://bit.ly/dan-abramov" />
-        </Box>
-        <Box>
-          <Image
-            boxSize={"200px"}
-            borderRadius={"full"}
-            src="https://bit.ly/dan-abramov"
-          />
-        </Box>
-        <Box>
-          <Image src="abcd.jpg" fallbackSrc="https://via.placeholder.com/150" />
-        </Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </Center>
-    </div>
+    <Flex
+      justifyContent={"space-between"}
+      py={"8px"}
+      my={"10px"}
+      bg={"gray.100"}
+      px={{ base: "0px", md: "70px", lg: "120px", xl: "480px" }}
+    >
+      <Flex gap={"7px"} align={"center"}>
+        <Text as={"b"}>Navbar</Text>
+        <Button variant={"ghost"}>Home</Button>
+        <Button variant={"ghost"}>Link</Button>
+        <Menu>
+          <MenuButton
+            variant={"ghost"}
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Action</MenuItem>
+            <MenuItem>Another action</MenuItem>
+
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+        <Button variant={"ghost"} isDisabled>
+          Disabled
+        </Button>
+      </Flex>
+      <Flex gap={"7px"} align={"center"}>
+        <Input size={"sm"} bg={"white"} />
+        <Button size={"sm"} variant={"outline"} colorScheme="green">
+          Searh
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
