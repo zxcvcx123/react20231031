f61496614f934bcef69e0510ffccab3e5191f553
JungMo <jmjm0611@naver.com>
2023-11-02 10:20:59 +0900

    차크라 ui 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor={"tomato"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"blue"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"red"}>Culpa, excepturi, quaerat.</Box>
+      <Box bgColor={"blue.100"}>Iusto, rerum, veritatis?</Box>
+      <Box bgColor={"blue.300"}>Excepturi, molestias, numquam.</Box>
+      <Box bgColor={"gray.400"} color={"yellow.100"}>
+        Deleniti, ducimus exercitationem?
+      </Box>
+      <Box bg={"facebook.500"} color={"linkedin.200"}>
+        Accusamus, atque, tempore.
+      </Box>
+      <Box>Dignissimos, ipsum magni?</Box>
+      <Box>Laborum, nesciunt possimus?</Box>
+      <Box>Deleniti, doloribus, molestias.</Box>
+      <Box>Accusantium, saepe, tenetur.</Box>
+    </>
+  );
 }
 
 export default App;
