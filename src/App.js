4781b6e90ce3dbddd255d4e21d401daed2b6956a
JungMo <jmjm0611@naver.com>
2023-11-03 16:52:36 +0900

    블로그 예제

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Center,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
+import React from "react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  let text = "";
 
-  function handleNameInput(e) {
-    setName(e.target.value);
-  }
-
-  function handlePasswordInput(e) {
-    setPassword(e.target.value);
-  }
-
-  function handleEmailInput(e) {
-    setEmail(e.target.value);
+  function changeText(e) {
+    text = e.target.value;
+    console.log(text);
   }
 
   return (
     <div>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>Name</FormLabel>
-            <Input type="text" value={name} onChange={handleNameInput} />
-            <FormHelperText>띄어쓰기 없이 입력해주세요.</FormHelperText>
-          </FormControl>
+      <input type="text" onChange={changeText} />
 
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input
-              type="password"
-              value={password}
-              onChange={handlePasswordInput}
-            />
-            <FormHelperText>
-              특수기호와 숫자를 하나 작성해주세요.
-            </FormHelperText>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" value={email} onChange={handleEmailInput} />
-
-            <FormHelperText>입력된 이메일은 중복될 수 없습니다.</FormHelperText>
-          </FormControl>
-
-          <Button colorScheme="blue">가입</Button>
-        </Box>
-      </Center>
+      <div
+        style={{ backgroundColor: "yellow", width: "100px", height: "50px" }}
+      >
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
