a6ea9aa100e9867bd18346fccb662c2004e1948a
JungMo <jmjm0611@naver.com>
2023-11-03 15:11:34 +0900

    usetState 예제

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Indicator, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
   let [number, setNumber] = useState(0);
-
+  let [text, setText] = useState("");
   function handleClick() {
     // number++;
-    setNumber(number + 1);
   }
   return (
     <div>
-      <Button onClick={handleClick}>바꾸기</Button>
+      <Button onClick={() => setNumber(number + 1)}>바꾸기</Button>
       <Text>{number}</Text>
+      <Input onChange={(e) => setText(e.target.value)}></Input>
+      <p>{text}</p>
     </div>
   );
 }
