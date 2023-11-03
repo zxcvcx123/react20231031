c28b5a69b31e6e212ce46fd33a469762ad6e8892
JungMo <jmjm0611@naver.com>
2023-11-03 09:19:47 +0900

    children 연습

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
-import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+function MyComp({ text, address, children }) {
+  return (
+    <>
+      <p>{text}</p>
+      <p>{address}</p>
+      <p>{children}</p>
+    </>
+  );
+}
 
 function App(props) {
   return (
-    <Flex
-      justifyContent={"space-between"}
-      py={"8px"}
-      my={"10px"}
-      bg={"gray.100"}
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "480px" }}
-    >
-      <Flex gap={"7px"} align={"center"}>
-        <Text as={"b"}>Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-        <Menu>
-          <MenuButton
-            variant={"ghost"}
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Action</MenuItem>
-            <MenuItem>Another action</MenuItem>
-
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-        <Button variant={"ghost"} isDisabled>
-          Disabled
-        </Button>
-      </Flex>
-      <Flex gap={"7px"} align={"center"}>
-        <Input size={"sm"} bg={"white"} />
-        <Button size={"sm"} variant={"outline"} colorScheme="green">
-          Searh
-        </Button>
-      </Flex>
-    </Flex>
+    <div>
+      <MyComp text={"hello"} address={"seoul"}>
+        some contents 다른 컴포넌트가 있을 수 도 있음.
+      </MyComp>
+    </div>
   );
 }
 
