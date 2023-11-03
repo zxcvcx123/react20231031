e68c53cda1a707afbc8197e1710177dd3e7b4213
JungMo <jmjm0611@naver.com>
2023-11-03 16:17:04 +0900

    usetState  상태 변경 응용 예제 + - 연습 완

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function App(props) {
   const [number, setNumber] = useState(0);
-  const isOdd = number % 2 == 1;
+
   return (
     <div>
-      <Input
-        type="number"
-        onChange={(e) => setNumber(Number(e.target.value))}
-      />
-      <Text>
-        {(isOdd && "홀수") || (!isOdd && "짝수")}: {number}
-      </Text>
+      <input type="number" value={number} />
+      <Button onClick={(e) => setNumber(number + 1)}>+</Button>
+      <Button onClick={(e) => setNumber(number - 1)}>-</Button>
     </div>
   );
 }
