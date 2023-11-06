6d058d92ddfd9e93e8adb0f17d22f8f9325c5d00
JungMo <jmjm0611@naver.com>
2023-11-06 10:37:08 +0900

    usestate re-render 조건이 값이 변경되어야 하는데 객체는 참조값을 사용하기에 변경이 안되는 문제가 발생할 수 잇음

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
+  // set... 메소드로 상태 변경할 수 있음
+  // 상태가 같은 지 아닌 지 판단해서 re-render 결정함
+
   const [number, setNumber] = useState(0);
-  const index = number % 3;
+  const [numberObject, setNumberObject] = useState({ number: 0 });
+
+  function handleNumberObjChange() {
+    numberObject.number = numberObject.number + 1;
+    setNumberObject(numberObject);
+  }
   return (
     <div>
-      <Button onClick={(e) => setNumber(number + 1)}>다음</Button>
-      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
-        <Text>message1</Text>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>number 변경</Button>
+        <Text>{number}</Text>
       </Box>
-      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
-        <Text>message2</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
-        <Text>message3</Text>
+      <Box>
+        <Button onClick={handleNumberObjChange}>number 객체 변경</Button>
+        <Text>{numberObject.number}</Text>
       </Box>
     </div>
   );
