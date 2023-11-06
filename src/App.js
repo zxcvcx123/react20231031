14f6d11f246417c26005df6f37df305b88ca1992
JungMo <jmjm0611@naver.com>
2023-11-06 10:22:32 +0900

    usestate 연습 완

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [text, setText] = useState("hello");
+  const [number, setNumber] = useState(0);
+  const index = number % 3;
   return (
     <div>
-      <Button
-        onClick={(e) => {
-          setText("상태 변경완료");
-        }}
-      >
-        상태 변경!
-      </Button>
-      <p>{text}</p>
+      <Button onClick={(e) => setNumber(number + 1)}>다음</Button>
+      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
+        <Text>message1</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
+        <Text>message2</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
+        <Text>message3</Text>
+      </Box>
     </div>
   );
 }
